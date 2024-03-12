86bd86bfff8720d846525940d74b54c9feae49cc
sanggyeol1 <126740335+sanggyeol1@users.noreply.github.com>
2024-03-12 23:59:24 +0900

    Add files via upload

---
+++ 프로그래머스/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,8 @@
+def solution(n):
+    arr = list()
+    for i in str(n):
+        arr.append(int(i))
+        
+    arr.reverse()
+    return arr
+    
---
+++ 프로그래머스/평균 구하기.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    sum = 0
+    for i in arr:
+        sum+=i
+    
+    answer = sum / len(arr)
+    
+    return answer
