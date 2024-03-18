890a741a25653bc464557db9c924aaae0cbf290f
sanggyeol1 <126740335+sanggyeol1@users.noreply.github.com>
2024-03-18 17:19:42 +0900

    Add files via upload

---
+++ 프로그래머스/문자열을 정수로 바꾸기.py
@@ -0,0 +1,9 @@
+def solution(s):
+        if s[0] == '-':
+            return int(s)
+        elif s[0] == '+':
+            return int(s)
+        else:
+            return int(s)
+            
+        
