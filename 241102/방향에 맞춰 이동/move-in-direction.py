02130c77ad4a3303b3ff5928fd466262c3657874
sanggyeol1 <126740335+sanggyeol1@users.noreply.github.com>
2024-11-02 17:55:22 +0900

    Title: 방향에 맞춰 이동, Time: 42ms, Memory: 15MB, Status: Passed - CodeTree

--- 241102/방향에 맞춰 이동/move-in-direction.py
+++ 241102/방향에 맞춰 이동/move-in-direction.py
@@ -1,19 +1,25 @@
-x, y = 0, 0
-
 n = int(input())
 
+x, y = 0, 0
+
+#동서남북
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
 for _ in range(n):
-    direction, length = map(str, input().split())
-    if direction == "N":
-        y = y + int(length)
-    
+    direction, distance = map(str, input().split())
+    distance = int(distance)
+
     if direction == "E":
-        x = x + int(length)
-    
-    if direction == "S":
-        y = y - int(length)
-    
-    if direction == "W":
-        x = x - int(length)
+        move_dir = 0
+    elif direction == "W":
+        move_dir = 1
+    elif direction == "S":
+        move_dir = 2
+    elif direction == "N":
+        move_dir = 3
+
+    x = x + dx[move_dir] * distance
+    y = y + dy[move_dir] * distance
 
 print(x, y)
